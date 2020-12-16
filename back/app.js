f1ac16568b78ba291c180959f1296b71f9f27b3f
Daniele Esposito <esp.dne@gmail.com>
2020-12-16 19:57:59 +0100

    connect MongoDB

--- back/app.js
+++ back/app.js
@@ -1,23 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());  
+app.use(bodyParser.json());
+//Connect to MongoDB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
+    console.log("connected to DB!");
+  });
 //ROUTES
 
 // GET Home
-app.get("/", (req, res) => {
-    res.send("THIS IS HOME");
-  });
+app.get("/", (request, response) => {
+  response.send("THIS IS HOME");
+});
 
-  //POST
-  app.post("/test", )
-
-
+//POST
+app.post("/test", (request, response) => {
+  console.log(response.body);
+  response.send("thanks for the request"); 
+});
 
 //Listen to the Server
 app.listen(3500);
