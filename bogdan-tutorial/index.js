b3f06c1dc7d510e8a31d08a80f9d3dc7b564db05
Daniele Esposito <esp.dne@gmail.com>
2020-12-26 07:14:53 +0100

    delete

--- bogdan-tutorial/index.js
+++ bogdan-tutorial/index.js
@@ -1,79 +1,91 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
+const { response } = require("express");
 
 const productList = [
-	{
-		id: "product_1",
-		name: "Milk",
-		price: 2
-	},
-	{
-		id: "product_2",
-		name: "Muffin",
-		price: 7
-	}
-]
+  {
+    id: "product_1",
+    name: "Milk",
+    price: 2,
+  },
+  {
+    id: "product_2",
+    name: "Muffin",
+    price: 7,
+  },
+];
 
-
-
-const port = 3000
+const port = 3000;
 
 app.use(bodyParser.json());
 
 // Store the product list in memory:
 
-app.get('/', (request, response) => {
-  response.send('This is the HomePage');
+app.get("/", (request, response) => {
+  response.send("This is the HomePage");
 });
 
-app.get('/productList', (request, response) => {
-    response.send(productList); 
-  });
+app.get("/productList", (request, response) => {
+  response.send(productList);
+});
 
 app.get("/products/:productId", (request, response) => {
-    // get the product Id from the request object
-    const productId = request.params.productId;
-    // filter the right product from the product list
-    const product = productList.find((prod) => prod.id == productId);
-    // return a 404 status if no product is found
-    if (!product) {
-      response.status(404).end();
+  // get the product Id from the request object
+  const productId = request.params.productId;
+  // filter the right product from the product list
+  const product = productList.find((prod) => prod.id == productId);
+  // return a 404 status if no product is found
+  if (!product) {
+    response.status(404).end();
+  }
+  // Send the product object as a JSON response to the client
+  response.json(product);
+});
+
+//POST
+
+app.post("/products/:productId", (request, response) => {
+  const product = request.body; //take the product
+  productList.push(product); // add product to product list
+  response.json(product); //send the product in the response
+});
+
+//PUT
+
+app.put("/products/:productId", (request, response) => {
+  const productId = request.params.productId;
+  const updatedProduct = request.body;
+
+  let newProduct = null;
+
+  productList.forEach((prod, index) => {
+    if (prod.id === productId) {
+      newProduct = {
+        ...productList[index],
+        ...updatedProduct,
+      };
+      productList[index] = newProduct;
     }
-    // Send the product object as a JSON response to the client
-    response.json(product);
   });
+  response.json(newProduct);
+});
 
-  //POST
+// DELETE
 
-  app.post('/products/:productId', (request, response) => {
-    const product = request.body;  //take the product
-    productList.push(product); // add product to product list
-    response.json(product); //send the product in the response
-  });
-
-  //PUT
-
-  app.put('/products/', (request, response) => {
-
+app.delete('/products/:productId', (request, response)=>{
     const productId = request.params.productId;
-    const updatedProduct = request.body;
 
-    productList.forEach((prod, index)=>{
-        if(prod.id === productId){
-productList[index] = {
-    
-}
-        }
-    })
-
-    const product = request.body;  
-    productList.push(product); 
-    response.json(product); 
+  productList.forEach((prod, index) => {
+    if (prod.id === productId) {
+      let newProduct = {};
+      productList[index] = newProduct;
+    }
   });
+  response.json(productList);
+});
 
-
-
+//Listen
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
